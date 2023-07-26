26232e2f2c88fb134cbff631e0220dacf4c3cd9c
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-25 19:39:50 -0700

    Done

--- _printf.c
+++ _printf.c
@@ -1,16 +1,10 @@
 #include "main.h"
-<<<<<<< HEAD
-#include <stddef.h>
-=======
-#include <stdarg.h>
-#include <unistd.h> /* for write */
->>>>>>> 5048d1c6c2bd24e7d12917158cfcf5b21b3fbd42
+#include <stddef.h> /* Add this line to include the NULL definition */
 
 /**
- * _printf - Produces output according to a format.
- * @format: A character string containing directives.
+ * _printf - Custom printf function
+ * @format: The format string
  *
-<<<<<<< HEAD
  * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
@@ -61,61 +55,5 @@ int _printf(const char *format, ...)
 
     va_end(args);
     return (printed_chars);
-=======
- * Return: The number of characters printed (excluding the null byte).
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int count = 0;
-	char *str_arg;
-	char ch;
-
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			count++;
-		}
-		else
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					ch = va_arg(args, int);
-					write(1, &ch, 1);
-					count++;
-					break;
-				case 's':
-					str_arg = va_arg(args, char *);
-					if (str_arg == NULL)
-						str_arg = "(null)";
-					while (*str_arg != '\0')
-					{
-						write(1, str_arg, 1);
-						str_arg++;
-						count++;
-					}
-					break;
-				case '%':
-					write(1, "%", 1);
-					count++;
-					break;
-				default:
-					write(1, format - 1, 2);
-					count += 2;
-					break;
-			}
-		}
-		format++;
-	}
-
-	va_end(args);
-
-	return (count);
->>>>>>> 5048d1c6c2bd24e7d12917158cfcf5b21b3fbd42
 }
+
